a8335f40f65b098d0837011ad5480c3362006e80
ugyuji <yuji.tsunomori@gmail.com>
2023-08-25 16:54:06 +0900

    Customize dashboard

--- chadrc.lua
+++ chadrc.lua
@@ -12,8 +12,20 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
-  
+
   telescope = { style = "bordered" },
+
+  nvdash = {
+    load_on_startup = true,
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    }
+  }
 }
 
 M.plugins = "custom.plugins"
