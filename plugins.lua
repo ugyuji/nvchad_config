6e508095a28d9fcca2e657ff1ab40b00c37c09c1
ugyuji <yuji.tsunomori@gmail.com>
2023-08-31 22:41:48 +0900

    Install wfxr/minimap.vim

--- plugins.lua
+++ plugins.lua
@@ -91,6 +91,16 @@ local plugins = {
   {
     "github/copilot.vim",
     event = "InsertEnter",
+  },
+
+  {
+    "wfxr/minimap.vim",
+    lazy = false,
+    init = function()
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+      vim.cmd("let g:minimap_width = 15")
+    end,
   }
 
   -- To make a plugin not be loaded
