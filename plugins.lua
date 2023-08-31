04e9a9b577092791069fcc98d1055ba73f673cd7
ugyuji <yuji.tsunomori@gmail.com>
2023-08-31 22:56:53 +0900

    Fix minimap width

--- plugins.lua
+++ plugins.lua
@@ -99,7 +99,7 @@ local plugins = {
     init = function()
       vim.cmd("let g:minimap_auto_start = 1")
       vim.cmd("let g:minimap_auto_start_win_enter = 1")
-      vim.cmd("let g:minimap_width = 15")
+      vim.cmd("let g:minimap_width = 10")
     end,
   }
 
