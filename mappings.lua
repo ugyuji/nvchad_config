99b352174dd2fe2720622f4e6d470fd6d61202c6
ugyuji <yuji.tsunomori@gmail.com>
2023-09-02 17:05:32 +0900

    Fix copilot
    https://azamuddin.com/en/blog/050623-setting-up-copilot-on-nvchad

--- mappings.lua
+++ mappings.lua
@@ -50,4 +50,16 @@ M.minimap = {
   },
 }
 
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+      end,
+      "Copilot Accept",
+      { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
+    },
+  },
+}
+
 return M
